38833d0b8a0860cb57e09201c0a5b0cbd9dc8641
Konstantin324 <k.tsonev@student.tue.nl>
2025-05-14 18:19:36 +0300

    Removed input_dataset test file

--- src/power_system_simulation/assignment_1.py
+++ src/power_system_simulation/assignment_1.py
@@ -156,7 +156,15 @@ class GraphProcessor:
         self.edge_vertex_id_pairs = edge_vertex_id_pairs
         self.edge_enabled = edge_enabled
         self.source_vertex_id = source_vertex_id
-        pass
+
+        # vertex_ids = [0, 2, 4, 6, 10]
+    # edge_ids = [1, 3, 5, 7, 8, 9]
+    # edge_vertex_id_pairs = [(0, 2), (0, 4), (0, 6), (2, 4), (2, 10), (4, 6)]
+    # edge_enabled = [True, True, True, False, True, True]
+    # source_vertex_id = 0
+
+    # g = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
+
 
     def find_downstream_vertices(self, edge_id: int) -> List[int]:
         """
--- tests/input_dataset.py
+++
@@ -1,7 +0,0 @@
-# vertex_ids = [0, 2, 4, 6, 10]
-# edge_ids = [1, 3, 5, 7, 8, 9]
-# edge_vertex_id_pairs = [(0, 2), (0, 4), (0, 6), (2, 4), (2, 10), (4, 6)]
-# edge_enabled = [True, True, True, False, True, True]
-# source_vertex_id = 0
-
-# g = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
