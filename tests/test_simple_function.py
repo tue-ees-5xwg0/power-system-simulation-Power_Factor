87756a4351254d349d99b2300a36927c51d49ec2
Konstantin324 <k.tsonev@student.tue.nl>
2025-05-14 18:26:58 +0300

    Imported modules in all test functions

--- tests/test_simple_function.py
+++ tests/test_simple_function.py
@@ -1,3 +1,9 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_array
+from scipy.sparse.csgraph import connected_components
+
 from power_system_simulation.assignment_1 import GraphProcessor
 from power_system_simulation.simple_function import add, multiply
 
