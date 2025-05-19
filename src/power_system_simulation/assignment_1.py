746758a36c759672c6ff6ae2e83a2775f37a3a2d
IsaakChristou <isaakchristou@gmail.com>
2025-05-19 14:35:19 +0200

    Update assignment_1.py

--- src/power_system_simulation/assignment_1.py
+++ src/power_system_simulation/assignment_1.py
@@ -164,13 +164,13 @@ class GraphProcessor(nx.Graph):
 
         # vertex_ids = [0, 2, 4, 6, 10]
 
-    # edge_ids = [1, 3, 5, 7, 8, 9]
-    # edge_vertex_id_pairs = [(0, 2), (0, 4), (0, 6), (2, 4), (2, 10), (4, 6)]
-    # edge_enabled = [True, True, True, False, True, True]
-    # source_vertex_id = 0
+        # edge_ids = [1, 3, 5, 7, 8, 9]
+        # edge_vertex_id_pairs = [(0, 2), (0, 4), (0, 6), (2, 4), (2, 10), (4, 6)]
+        # edge_enabled = [True, True, True, False, True, True]
+        # source_vertex_id = 0
 
-    # g = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+        # g = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
+
         # Build enabled subgraph for fast queries
         self.enabled_subgraph = nx.Graph()
         for (u, v), enabled in zip(edge_vertex_id_pairs, edge_enabled):
@@ -237,7 +237,6 @@ class GraphProcessor(nx.Graph):
         descendants = list(nx.descendants(self.dfs_tree, downstream_root))
         print([downstream_root] + descendants)
         return [downstream_root] + descendants
-    
 
     def find_alternative_edges(self, disabled_edge_id: int) -> List[int]:
         """
